77f14cdc06f0c3f8e0018be043d4458fe462bf8e
ahhan02 <xiaomeng.han@bianlifeng.com>
2019-03-10 20:21:33 +0800

    Update Q561

--- Q561_arrayPairSum.cpp
+++ Q561_arrayPairSum.cpp
@@ -2,12 +2,36 @@
 #include <vector>
 using namespace std;
 
-int arrayPairSum(vector<int>& nums) {
+int arrayPairSum1(vector<int>& nums) {
   sort(nums.begin(), nums.end());
 
   int res = 0;
   for (int i = 0; i < nums.size(); i+=2) {
     res += nums[i];
+  }
+
+  return res;
+}
+
+int exist[20001] = { 0 };
+int arrayPairSum(vector<int>& nums) {
+  // int *exist = new int[20001];
+  // int exist[20001] = { 0 };
+  for (int i = 0; i < nums.size(); ++i) {
+    exist[nums[i] + 10000]++;
+  }
+
+  int res = 0;
+  bool odd = true;
+  for (int i = 0; i < 20001; ++i) {
+    while (exist[i]) {
+      if (odd) {
+        res += i - 10000;
+      }
+
+      odd = !odd;
+      exist[i]--;
+    }
   }
 
   return res;
